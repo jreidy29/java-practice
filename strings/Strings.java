49aacb8f15c613d72947b034cf56d6b7b96665b3
Jack Reidy <jnreidy15@gmail.com>
2019-09-30 22:53:34 -0700

    more strings practice

--- strings/Strings.java
+++ strings/Strings.java
@@ -13,11 +13,58 @@ public class Strings {
     return true;
   }
 
+  public String substring(String input, int a, int b) {
+    StringBuilder ret = new StringBuilder();
+    for(int i = a; i < b; i++) {
+      ret = ret.append(input.charAt(i));
+    }
+    return ret.toString();
+  }
+
+  //Return smallest and largest k-length substring of given string
+  //
+  public String getSmallestAndLargest(String s, int k) {
+    String smallest = "";
+    String largest = "";
+    smallest = s.substring(0, k);
+    largest = s.substring(0, k);
+    k++;
+    for(int i = 1; k - 1 < s.length(); i++) {
+       if(smallest.compareTo(s.substring(i, k)) > 0 ) {
+         smallest = s.substring(i, k);
+       } 
+       if(largest.compareTo(s.substring(i, k)) < 0) {
+         largest = s.substring(i, k); 
+       }
+       k++;
+    }
+    return smallest + "\n" + largest;
+  }
+
+  public boolean testSubstring() {
+    String test = "Helloworld";
+    int a = 3;
+    int b = 7;
+    String success = "lowo";
+    Strings testing = new Strings();
+    return success.equals(testing.substring(test, a, b));
+  }
+
+  public boolean testSaL() {
+    String s = "welcometojava";
+    int k = 3;
+    String ans = "ava\nwel";
+    Strings testing = new Strings();
+    return ans.equals(testing.getSmallestAndLargest(s, k));
+  }
+
   public static void main(String[] args) {
     Strings testStrings = new Strings();
-    System.out.println(testStrings.uniqueString("does this count as unique"));
-    System.out.println(testStrings.uniqueString("abcdefghijklmnopqrstuvwxyz"));
-    System.out.println(testStrings.uniqueString(""));
-    System.out.println(testStrings.uniqueString("123456789987654321"));
+    //System.out.println(testStrings.uniqueString("does this count as unique"));
+    //System.out.println(testStrings.uniqueString("abcdefghijklmnopqrstuvwxyz"));
+    //System.out.println(testStrings.uniqueString(""));
+    //System.out.println(testStrings.uniqueString("123456789987654321"));
+    System.out.println("testing substring passes: " + testStrings.testSubstring());
+    System.out.println("testing SaL passes: " + testStrings.testSaL());
   }
 }
